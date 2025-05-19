25e83485ecf92bfa4c391935da3f3c8fc09f5cea
johann219 <147496124+johann219@users.noreply.github.com>
2025-05-19 23:15:46 +0300

    Fix magic values in render-todo.js

--- src/js/render-todo.js
+++ src/js/render-todo.js
@@ -1,18 +1,24 @@
-const todoTemplate = document.querySelector('.todo-item-template');
-const todoList = document.querySelector('.todo-list');
+const TODO_LIST_ELEMENT_CLASS = '.todo-list';
+const TODO_ITEM_ELEMENT_CLASS = '.todo-item';
+const TODO_ITEM_TEMPLATE_CLASS = '.todo-item-template';
+const TODO_ITEM_TITLE_CLASS = '.todo-title';
+const TODO_ITEM_STATUS_CLASS = '.todo-status';
+
+const todoTemplate = document.querySelector(TODO_ITEM_TEMPLATE_CLASS);
+const todoList = document.querySelector(TODO_LIST_ELEMENT_CLASS);
 
 const renderTodo = (todo) => {
     const newTodoElement = todoTemplate.content.cloneNode(true);
     
-    const newTodoTitle = newTodoElement.querySelector('.todo-title');
+    const newTodoTitle = newTodoElement.querySelector(TODO_ITEM_TITLE_CLASS);
     newTodoTitle.textContent = todo.title;
 
     if (todo.isCompleted) {
-        const newTodoStatus = newTodoElement.querySelector('.todo-status');
+        const newTodoStatus = newTodoElement.querySelector(TODO_ITEM_STATUS_CLASS);
         newTodoStatus.setAttribute('checked', '');
     }
 
-    newTodoElement.querySelector('.todo').id = todo.id;
+    newTodoElement.querySelector(TODO_ITEM_ELEMENT_CLASS).setAttribute('id', `${todo.id}`);
 
     todoList.appendChild(newTodoElement);
 }
