f85f898ccc6d0ad88a50aa8d902e5eaf351a91e7
johann219 <147496124+johann219@users.noreply.github.com>
2025-06-03 15:41:37 +0300

    Extend const declarations

--- src/js/const.js
+++ src/js/const.js
@@ -7,18 +7,33 @@ const TODO_PROPERTY_TYPE = {
 const SELECTOR = {
     ADD_TODO_BUTTON: '.add-todo',
     TODO_LIST: '.todo-list',
+
+    TODO_ITEM_TEMPLATE: '.todo-form-template',
     TODO_ITEM_ELEMENT: '.todo-item',
-    TODO_ITEM_TEMPLATE: '.todo-item-template',
-    TODO_ITEM_CHECKBOX: '.todo-status',
-    TODO_ITEM_DELETE_BTN: '.todo-delete',
-    TODO_ITEM_STATUS: '.todo-status',
+    TODO_CONTENT_WRAPPER: '.todo-content-wrapper',
     TODO_ITEM_TITLE: '.todo-title',
-    // TODO_ITEM_DESCRIPTION: '.todo-description',
-    // TODO_ITEM_DUEDATE: '.todo-duedate',
+    TODO_ITEM_DESCRIPTION: '.todo-description',
+    TODO_ITEM_DATETIME: '.todo-datetime',
+    TODO_BUTTON_WRAPPER: '.todo-button-wrapper',
+    TODO_BUTTON_COMPLETE: '.todo-complete',
+    TODO_BUTTON_CANCEL: '.todo-cancel',
+
+    TODO_FORM_TEMPLATE: '.todo-form-template',
+    TODO_FORM: '.todo-form',
+    TODO_FORM_CONTENT_WRAPPER: '.todo-form-content-wrapper',
+    TODO_FORM_TITLE_INPUT: '.todo-form-title',
+    TODO_FORM_DESCRIPTION_INPUT: '.todo-form-description',
+    TODO_FORM_DATETIME_WRAPPER: '.todo-form-datetime-wrapper',
+    TODO_FORM_DATETIME_ICON: '.todo-form-datetime-icon',
+    TODO_FORM_DATETIME_DISPLAY: '.todo-form-datetime-display',
+    TODO_FORM_DATETIME_INPUT: '.todo-form-datetime-input',
+    TODO_FORM_BUTTON_WRAPPER: '.todo-form-button-wrapper',
+    TODO_FORM_BUTTON_CONFIRM: '.todo-form-confirm',
+    TODO_FORM_BUTTON_CANCEL: '.todo-form-cancel',
 }
 
 const MARKUP_CLASS = {
     TODO_ITEM_COMPLETION: 'todo-completed',
 }
 
-export { TODO_PROPERTY_TYPE, SELECTOR, MARKUP_CLASS };
+export {TODO_PROPERTY_TYPE, SELECTOR, MARKUP_CLASS};
